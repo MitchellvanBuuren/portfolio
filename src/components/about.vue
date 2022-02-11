e9fe2c9aaba7d1e8441fec84bf63eda5269a3abd
Mitchell van Buuren <mitchellvb@uprate.co.za>
2022-02-11 09:21:07 +0200

    added fix to responsiveness of text && removed resume button

--- src/components/about.vue
+++ src/components/about.vue
@@ -3,30 +3,29 @@
     <v-card-title class="display-1">
       Mitchell van Buuren
     </v-card-title>
-    <div v-bind:class="{ 'd-flex flex-no-wrap justify-space-between': !isMobile, '': isMobile } ">
-      <v-avatar
-          class="ma-3"
-          size="150"
-      >
-        <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
-      </v-avatar>
-      <v-card-text class="text-body-1">
-        First of all, I love challenges. Ever since I solved my first bug I knew
-        that coding is my passion. I believe that programming transforms the world around us.
-        I started out as a junior software engineer. Reading code, attempting bug fixes, failing at bug fixes and adding
-        a
-        button here and there. Slowly building my knowledge base and experience. Fast forward to today - I am a full
-        stack intermediate software engineer.
-        <br>
-        <br>
-        <a :href="`${path}resume.pdf`" download="resume.pdf">
-          <v-btn color="primary">
-            <v-icon>mdi-download</v-icon>
-            Resume
-          </v-btn>
-        </a>
-      </v-card-text>
-    </div>
+    <v-row>
+      <v-col class="text-center" cols="12" md="2">
+        <v-avatar
+            class="ma-auto"
+            size="150"
+        >
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
+        </v-avatar>
+      </v-col>
+      <v-col cols="12" md="10" class="text-center">
+        <v-card-text class="text-body-1">
+          Ever since I solved my first bug I knew
+          that coding is my passion. I believe that programming transforms the world around us.
+          I started out as a junior software engineer. Reading code, attempting bug fixes, failing at bug fixes and
+          adding
+          a
+          button here and there. Slowly building my knowledge base and experience. Fast forward to today - I am a full
+          stack intermediate software engineer.
+          <br>
+          <br>
+        </v-card-text>
+      </v-col>
+    </v-row>
     <v-card-title class="display-1">
       Skills
     </v-card-title>
@@ -48,39 +47,39 @@
       </v-list>
     </v-card-text>
     <v-row>
-    <v-card-title class="display-1">
-      Methodologies
-    </v-card-title>
-    <v-card-text>
-      <v-list color="background">
-        <v-list-item v-for="(methodology, idx) in methodologies" :key="idx">
-          <v-list-item-icon>
-            <v-icon>mdi-circle-small</v-icon>
-          </v-list-item-icon>
-          {{ methodology }}
-        </v-list-item>
-      </v-list>
-    </v-card-text>
-    <v-card-title class="display-1">
-      Bio
-    </v-card-title>
-    <v-card-text class="text-body-1">
-      <strong>1997</strong> - Born in Germiston, South Africa.
-      <br>
-      <br>
-      <strong>2015</strong> - Matriculated at Ho&euml;rskool Brandwag.
-      <br>
-      <br>
-      <strong>2018</strong> - Studied Chemical Engineering (2nd year) at the University of Pretoria.
-      <br>
-      <br>
-      <strong>2020</strong> - Started at Uprate Technologies as a junior software engineer.
-      <br>
-      <br>
-      <strong>2021</strong> - Promoted to intermediate software engineer and dev team lead at Uprate Technologies.
-      <br>
-      <br>
-    </v-card-text>
+      <v-card-title class="display-1">
+        Methodologies
+      </v-card-title>
+      <v-card-text>
+        <v-list color="background">
+          <v-list-item v-for="(methodology, idx) in methodologies" :key="idx">
+            <v-list-item-icon>
+              <v-icon>mdi-circle-small</v-icon>
+            </v-list-item-icon>
+            {{ methodology }}
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+      <v-card-title class="display-1">
+        Bio
+      </v-card-title>
+      <v-card-text class="text-body-1">
+        <strong>1997</strong> - Born in Germiston, South Africa.
+        <br>
+        <br>
+        <strong>2015</strong> - Matriculated at Ho&euml;rskool Brandwag.
+        <br>
+        <br>
+        <strong>2018</strong> - Studied Chemical Engineering (2nd year) at the University of Pretoria.
+        <br>
+        <br>
+        <strong>2020</strong> - Started at Uprate Technologies as a junior software engineer.
+        <br>
+        <br>
+        <strong>2021</strong> - Promoted to intermediate software engineer and dev team lead at Uprate Technologies.
+        <br>
+        <br>
+      </v-card-text>
     </v-row>
   </v-card>
 </template>
